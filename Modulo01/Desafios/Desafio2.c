dd8ce5be53962593f3d5a5a30db87e509c12df13
Kenny Robert Gomes Da Silva <2006315@aluno.univesp.br>
2022-02-13 18:31:26 -0300

    Desafio2

---
+++ Modulo01/Desafios/Desafio2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+void main(){
+        setlocale(LC_ALL, "Portuguese");
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        printf("\\a \t= \tBeep\n");
+        printf("\\n \t= \tNova linha\n");
+        printf("\\t \t= \tTabulação\n");
+        printf("\\\\ \t= \tBarra\n");
+        printf("%%%% \t= \tPorcentagem\n");
+        printf("\\\? \t= \tInterrogação\n");
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+}
