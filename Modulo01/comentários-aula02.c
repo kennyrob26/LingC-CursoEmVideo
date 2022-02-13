58237070d78d28ddb64c9a5460414d94a55a9704
Kenny Robert Gomes Da Silva <2006315@aluno.univesp.br>
2022-02-13 19:12:50 -0300

    Comentários

---
+++ Modulo01/comentários-aula02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <locale.h>
+void main(){
+        //Um exemplo de comentário de única linha
+
+        /*e aqui encontramos um
+         cometário de multiplas
+         linhas
+         */
+
+}
