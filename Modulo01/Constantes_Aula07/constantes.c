d987d7b0a082cb6140ec6654b3c6d551d322b5b1
Kenny Robert Gomes Da Silva <2006315@aluno.univesp.br>
2022-03-18 19:22:09 -0300

    constante

---
+++ Modulo01/Constantes_Aula07/constantes.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+#define SAQUE_MAX 1000  //constante com #define
+void main(){
+
+    const int idade = 25;  //constante com const
+
+    printf("O valor maximo que pode ser sacado e de: %d \n", SAQUE_MAX);
+    printf("A idade minima permitida e: %d \n", idade);
+
+}
